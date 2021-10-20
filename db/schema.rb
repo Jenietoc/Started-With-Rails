b504b932bcc3d68b5a6d72428b537d2a7dde9fa2
Juan Esteban Nieto <juan.esteban.0505@hotmail.com>
2021-10-20 11:08:31 -0500

    Add tailwind in the Blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_204250) do
+ActiveRecord::Schema.define(version: 2021_10_20_142150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
